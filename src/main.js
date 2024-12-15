47f6e4f79764c064ea18e3ab4fc6bda3a587e747
ErosVillarroel <eros.vilaroel@gmail.com>
2024-12-15 11:32:01 -0400

    te quiero mucho amor

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import "./style.css";
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
-const spaceBackground = new THREE.TextureLoader().load("/images/b-w-fog.jpg");
+const spaceBackground = new THREE.TextureLoader().load(
+  "/images/black-backdrop.webp"
+);
 scene.background = spaceBackground;
 
 //Camara
@@ -14,7 +16,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.y = 70;
+camera.position.y = 140;
 camera.lookAt(0, 0, 0);
 
 //renderer
@@ -25,52 +27,77 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 //luces
-const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+const ambientLight = new THREE.AmbientLight(0xdfe9f3, 0.5);
 
 //niebla:
-scene.fog = new THREE.Fog(0xdfe9f3, 60, 100);
+scene.fog = new THREE.Fog(0x2e3132, 40, 110);
 
 //GRID
 const gridHelper = new THREE.GridHelper(window.innerWidth, 120);
 
 //Controles
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+let glassIsocahedrum = null;
+let glassIsocahedrum1 = null;
+let glassCube = null;
 
 function createCubesArray() {
   // Geometría de un cubo básico
   const sideSize = 14;
-  const cubeGeometry = new THREE.BoxGeometry(sideSize, sideSize, sideSize);
-  const basicCubeMaterial = new THREE.MeshStandardMaterial({ color: 0xff5733 });
-
-  const columns = 16;
-  const rows = 8;
+  const columns = 25;
+  const rows = 15;
 
   const separation = 2;
 
-  const gridInitialPosX = -118; // Posición inicial de la grid en x
-  const gridInitialPosZ = -55; // Posición inicial de la grid en z
+  const gridInitialPosX = -180; // Posición inicial de la grid en x
+  const gridInitialPosZ = -100; // Posición inicial de la grid en z
   const gridInitialPosY = -7; // Posición inicial de la grid en y
 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
-      const basicCube = new THREE.Mesh(cubeGeometry, basicCubeMaterial);
+      //Buttons
 
-      // Ajustar las posiciones iniciales
-      basicCube.position.x = gridInitialPosX + j * (sideSize + separation);
-      basicCube.position.z = gridInitialPosZ + i * (sideSize + separation);
-      basicCube.position.y = gridInitialPosY;
+      let randomHelp = THREE.MathUtils.randInt(0, 100);
+      //Background cubes
+      if (randomHelp < 45) {
+      } else {
+        const cubeGeometry = new THREE.BoxGeometry(
+          sideSize,
+          THREE.MathUtils.randInt(0, 70),
+          sideSize
+        );
+        const basicCubeMaterial = new THREE.MeshBasicMaterial({
+          color: 0x949596,
+        });
+        const basicCube = new THREE.Mesh(cubeGeometry, basicCubeMaterial);
 
-      scene.add(basicCube);
+        // Ajustar las posiciones iniciales
+        basicCube.position.x = gridInitialPosX + j * (sideSize + separation);
+        basicCube.position.z = gridInitialPosZ + i * (sideSize + separation);
+        basicCube.position.y = gridInitialPosY;
+
+        scene.add(basicCube);
+      }
     }
   }
 }
 
+let progress = 0;
 //ANADIR A ESCENA
-scene.add(ambientLight, gridHelper);
+scene.add(ambientLight);
 createCubesArray();
 function animate() {
-  controls.update();
+  // controls.update();
 
+  if (progress < 1000) {
+    progress++;
+  } else {
+    progress += 0.7;
+  }
+
+  // const time = progress * 0.0001;
+  // camera.rotation.z = Math.sqrt(time % 10);
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
